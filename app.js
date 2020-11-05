eddaf7e0a6ca09dae29da995940eda61b99c03b0
Sebastian Flor <sflor@telecomboutique.com>
2020-11-05 16:26:00 +0100

    feat(app.js): req and res parameters of a petition

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const handler = (req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    //proccess.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>hi</title></head><body><h1>Hello world!</h1></body>');
+    res.write('</html>');
+    res.end();
 }
 
 const server = http.createServer(handler);
