84e3028b9600361bb6c9063b9fcc0f9bbb8c8ba3
Sebastian Flor <sflor@telecomboutique.com>
2020-11-17 20:42:29 +0100

    feat(views): serve html pages and filter paths

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
@@ -11,7 +12,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+
+// 404 Error Page
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
